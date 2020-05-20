dd61594e90db87138c2583fa6c05309c4ab578ee
carlos.rojas-matas <carlos.rojas-matas@klarna.com>
2020-05-20 08:36:05 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,127 @@
-import React from 'react';
+import React, { useState,setState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
+import Radium,{StyleRoot} from 'radium';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = ( props ) =>  {
+  const [personState, setPersonState]= useState(
+    {
+      persons:[
+        {id:"1",name:"Juan",age:"18"},
+        {id:"2",name:"Pedro",age:"33"},
+        {id:"3",name:"Maria",age:"60"}
+      ],
+      showPersons:true
+    }
+    )
+    
+    const [dummyState,setDummyState]=useState("dummy string");
+    
+    // const switchNameHandler = (newName) =>{
+    //   setPersonState({n
+    //     persons:[
+    //       {id:"person1",name:newName,age:"22"},
+    //       {id:"person2",name:"Fran",age:"44"},
+    //       {id:"person3",name:"Daniela",age:"34"}
+    //     ]
+    //   })
+    // }
+    
+    const nameChangeHandler = (event, id) =>{
+      const pidx = personState.persons.findIndex(p => p.id === id);
+      const p = {...personState.persons[pidx]};
+      //const p = Object.assign({},personState.persons[pidx]);
+      p.name = event.target.value; 
+      
+      const persons = [...personState.persons];
+      persons[pidx] = p;
+      setPersonState(
+        {persons:persons,
+        showPersons:personState.showPersons});
+      
+    };
+    
+    const deletePersonHandler = (index) => {
+      const p = [...personState.persons];
+      p.splice(index,1);
+      setPersonState({
+        persons:p,
+        showPersons:personState.showPersons
+      });
+    }
+    
+    const togglePersonsHandler = ()=>{
+      const currState = personState.showPersons;
+      setPersonState({
+        persons:personState.persons,
+        showPersons:!currState});
+      }
+      
+      const buttonStyle = {
+        backgroundColor: 'green',
+        color:'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer',
+        ':hover':{
+          backgroundColor: 'lightgreen',
+          color: 'black'
+        }
+      };
+      
+      const evalPersons = () => {
+        let personsDiv = null;
+        if(personState.showPersons){
+          personsDiv = (
+            <div>
+            {personState.persons.map((per,idx) => {
+              return <Person 
+              name={per.name} 
+              age = {per.age}
+              deleteHandler = {() => deletePersonHandler(idx)}
+              changeName = {(event) => nameChangeHandler(event,per.id)}
+              key={per.id}>I play futbol
+              
+              </Person>
+            })}
+            </div>
+            );
+            buttonStyle.backgroundColor='red';
+            buttonStyle[':hover'] = {
+              backgroundColor: 'salmon',
+              color: 'black'
+            }
+          }
+          
+          return personsDiv;
+        }
 
-export default App;
+        let classes= [];
+        
+        if(personState.persons.length <= 2){
+          classes.push('red');
+        }
+
+        if(personState.persons.length <= 1){
+          classes.push('bold');
+        }
+
+        return(
+            <StyleRoot>
+              <div className="App">
+                <h1>I'm a react app</h1>
+                <p className={classes.join(' ')}>This is working</p>
+                <button 
+                  style={buttonStyle}  
+                  onClick={togglePersonsHandler}>Toggle persons</button>
+                {evalPersons()}
+              </div>
+            </StyleRoot>
+          );
+          
+        }
+        
+export default Radium(App);
+        
